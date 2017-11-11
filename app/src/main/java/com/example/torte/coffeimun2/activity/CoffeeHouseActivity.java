e67008d3475ba557cf30df478d105738fead68c0
Dmitry Yunkov <dima.yonkov@gmail.com>
2017-11-11 15:44:51 +0300

    coffe house dialog

--- app/src/main/java/com/example/torte/coffeimun2/activity/CoffeeHouseActivity.java
+++ app/src/main/java/com/example/torte/coffeimun2/activity/CoffeeHouseActivity.java
@@ -1,10 +1,19 @@
 package com.example.torte.coffeimun2.activity;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.torte.coffeimun2.R;
@@ -42,11 +51,36 @@ public class CoffeeHouseActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         ArrayList<CreateList> createLists = prepareData();
         RecyclerViewClickListener listener = (view, position) -> {
-            Toast.makeText(getApplicationContext(), "Position " + position , Toast.LENGTH_SHORT).show();
+            showChangeLangDialog(position);
         };
         CoffeHouseAdapter adapter = new CoffeHouseAdapter(getApplicationContext(), createLists, listener);
         recyclerView.setAdapter(adapter);
     }
+
+    public void showChangeLangDialog(Integer id) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = this.getLayoutInflater();
+        final View dialogView = inflater.inflate(R.layout.custom_dialog, null);
+        dialogBuilder.setView(dialogView);
+        TextView dialogTextContent = (TextView)dialogView.findViewById(R.id.dialog_text);
+        ImageView dialogImage = (ImageView)dialogView.findViewById(R.id.dialog_image);
+        dialogBuilder.setTitle("Custom Dialog Box");
+        dialogImage.setImageResource(this.image_ids[id]);
+        dialogBuilder.setPositiveButton("Заказать", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                Toast.makeText(getApplicationContext(), "OKKKK" , Toast.LENGTH_SHORT).show();
+            }
+        });
+        dialogBuilder.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                Toast.makeText(getApplicationContext(), "Отменить" , Toast.LENGTH_SHORT).show();
+            }
+        });
+        AlertDialog b = dialogBuilder.create();
+        b.show();
+    }
+
     private ArrayList<CreateList> prepareData(){
 
         ArrayList<CreateList> theimage = new ArrayList<>();
